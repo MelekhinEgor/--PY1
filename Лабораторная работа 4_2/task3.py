c9ecf88d6f72789f60a75e8aea3368d7247634fa
Egor <118932053+MelekhinEgor@users.noreply.github.com>
2022-12-03 23:36:42 +0300

    Лабораторная работа 4_2

---
+++ Лабораторная работа 4_2/task3.py
@@ -0,0 +1,13 @@
+def delete(list_, index=None):
+    if index is None:  # проверяем задан ли индекс, если нет ставим значение по умолчанию -1
+        index = -1
+        modified_list = list_[:index]
+    else:  # если индекс задан то слайсируем список на два - до и после индекса
+        modified_list = list_[:index] + list_[(index + 1):]  # объединяем списки в один итоговый
+    return modified_list
+
+
+print(delete([0, 1, 2], index=0))  # [1, 2]
+print(delete([0, 1, 2], index=1))  # [0, 2]
+print(delete([0, 1, 2]))  # [0, 1]
+# тут должна быть пустая строка
